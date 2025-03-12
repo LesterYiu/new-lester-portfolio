8342a756ed30396dd8bd0cb4ba4be9668ca852d6
LesterYiu <lester.ws.yiu@gmail.com>
2025-03-12 19:14:51 -0400

    Initial commit

---
+++ app/utility/contentful-query.tsx
@@ -0,0 +1,37 @@
+export const POST_GRAPHQL_FIELDS = `
+  slug
+  title
+  coverImage {
+    url
+  }
+  date
+  author {
+    name
+    picture {
+      url
+    }
+  }
+  excerpt
+  content {
+    json
+    links {
+      assets {
+        block {
+          sys {
+            id
+          }
+          url
+          description
+        }
+      }
+    }
+  }
+`;
+
+export const HEADER_GRAPHQL_FIELDS = `
+  title
+  links {
+    title
+    link
+  }
+`
--- lib/api.ts
+++ lib/api.ts
@@ -1,32 +1,4 @@
-const POST_GRAPHQL_FIELDS = `
-  slug
-  title
-  coverImage {
-    url
-  }
-  date
-  author {
-    name
-    picture {
-      url
-    }
-  }
-  excerpt
-  content {
-    json
-    links {
-      assets {
-        block {
-          sys {
-            id
-          }
-          url
-          description
-        }
-      }
-    }
-  }
-`;
+import { POST_GRAPHQL_FIELDS, HEADER_GRAPHQL_FIELDS } from "@/app/utility/contentful-query"
 
 async function fetchGraphQL(query: string, preview = false): Promise<any> {
   return fetch(
@@ -47,29 +19,29 @@ async function fetchGraphQL(query: string, preview = false): Promise<any> {
   ).then((response) => response.json());
 }
 
-function extractPost(fetchResponse: any): any {
-  return fetchResponse?.data?.postCollection?.items?.[0];
-}
+// function extractPost(fetchResponse: any): any {
+//   return fetchResponse?.data?.postCollection?.items?.[0];
+// }
 
-function extractPostEntries(fetchResponse: any): any[] {
-  return fetchResponse?.data?.postCollection?.items;
-}
+// function extractPostEntries(fetchResponse: any): any[] {
+//   return fetchResponse?.data?.postCollection?.items;
+// }
 
-export async function getPreviewPostBySlug(slug: string | null): Promise<any> {
-  const entry = await fetchGraphQL(
-    `query {
-      postCollection(where: { slug: "${slug}" }, preview: true, limit: 1) {
-        items {
-          ${POST_GRAPHQL_FIELDS}
-        }
-      }
-    }`,
-    true,
-  );
-  return extractPost(entry);
-}
+// export async function getPreviewPostBySlug(slug: string | null): Promise<any> {
+//   const entry = await fetchGraphQL(
+//     `query {
+//       postCollection(where: { slug: "${slug}" }, preview: true, limit: 1) {
+//         items {
+//           ${POST_GRAPHQL_FIELDS}
+//         }
+//       }
+//     }`,
+//     true,
+//   );
+//   return extractPost(entry);
+// }
 
-export async function getAllPosts(isDraftMode: boolean): Promise<any[]> {
+export async function getHomePage (isDraftMode: boolean): Promise<any> {
   const entries = await fetchGraphQL(
     `query {
       postCollection(where: { slug_exists: true }, order: date_DESC, preview: ${
@@ -79,42 +51,17 @@ export async function getAllPosts(isDraftMode: boolean): Promise<any[]> {
           ${POST_GRAPHQL_FIELDS}
         }
       }
+      headerCollection(preview: ${isDraftMode ? "true" : "false"}) {
+        items {
+          ${HEADER_GRAPHQL_FIELDS}
+        }
+      }
     }`,
     isDraftMode,
   );
-  return extractPostEntries(entries);
-}
 
-export async function getPostAndMorePosts(
-  slug: string,
-  preview: boolean,
-): Promise<any> {
-  const entry = await fetchGraphQL(
-    `query {
-      postCollection(where: { slug: "${slug}" }, preview: ${
-        preview ? "true" : "false"
-      }, limit: 1) {
-        items {
-          ${POST_GRAPHQL_FIELDS}
-        }
-      }
-    }`,
-    preview,
-  );
-  const entries = await fetchGraphQL(
-    `query {
-      postCollection(where: { slug_not_in: "${slug}" }, order: date_DESC, preview: ${
-        preview ? "true" : "false"
-      }, limit: 2) {
-        items {
-          ${POST_GRAPHQL_FIELDS}
-        }
-      }
-    }`,
-    preview,
-  );
   return {
-    post: extractPost(entry),
-    morePosts: extractPostEntries(entries),
+    posts: entries?.data?.postCollection?.items || [],
+    headers: entries?.data?.headerCollection?.items || [],
   };
 }
