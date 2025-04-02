e663c73908e5fa8470d7eec31be3b575c7eed673
LesterYiu <lester.ws.yiu@gmail.com>
2025-04-02 19:55:36 -0400

    Added form submit api route, added email js node.js ver, added form validation/logic

---
+++ app/api/form-submit/route.js
@@ -0,0 +1,53 @@
+import emailjs from '@emailjs/nodejs';
+
+export async function POST ( req ) {
+
+    const { firstName, lastName, email, emailMessage } = await req.json();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const invalidValuesObj = {};
+
+    function validateForm( emailAddress ) {
+
+
+        if ( firstName.length <= 1 ) invalidValuesObj.firstName = true;
+
+        if ( emailMessage.length <= 10 ) invalidValuesObj.emailMessage = true;
+
+        if ( ! emailRegex.test( emailAddress ) ) invalidValuesObj.emailAddress = true;
+
+        return Object.values(invalidValuesObj).some(val => val === true);
+
+    }
+
+    if ( ! validateForm( email ) ) {
+        return Response.json({ success: true, message: "Email sent successfully!" }, { status: 200 });
+    } else {
+        return Response.json({ success: false, message: "Email has not been sent.", invalidValues : invalidValuesObj}, { status: 500 });
+    }
+
+    // try {
+    //     emailjs.send( 
+    //         process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID, 
+    //         process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID, 
+    //         { 
+    //             firstName,
+    //             lastName,
+    //             email,
+    //             emailMessage
+    //         }, 
+    //         {
+    //             publicKey : process.env.NEXT_PUBLIC_EMAIL_API_KEY,
+    //             privateKey : process.env.NEXT_PRIVATE_EMAIL_API_KEY
+    //         }
+    //     )
+    //     .then( ( res ) => {
+    //         console.log('test')
+    //     })
+    //     return Response.json({ success: true, message: "Email sent successfully!" }, { status: 200 });
+        
+    // } catch ( error ) {
+    //     console.log( error)
+    // }
+}
--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,13 +1,55 @@
+'use client'
+
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { SendSvg, StarSvg } from "../utility/svgs";
 import { ResultObj } from "../utility/types";
 import { options } from '@/app/utility/contentful-richtext';
+import { FormEvent, use, useEffect, useState } from "react";
 
 const Form = ({
     title,
     subheading,
     formDescription
 }: ResultObj) => {
+
+    const [ firstName, setFirstName ] = useState( '' );
+    const [ lastName, setLastName ] = useState( '' );
+    const [ email, setEmail ] = useState( '' );
+    const [ emailMessage, setEmailMessage ] = useState( '' );
+
+    const [ isFirstNameInvalid, setIsFirstNameInvalid ] = useState( false );
+    const [ isEmailInvalid, setIsEmailInvalid ] = useState( false );
+    const [ isMessageInvalid, setIsMessageInvalid ] = useState( false );
+
+
+    const onFormSubmit = async ( e : FormEvent<HTMLFormElement> ) => {
+
+        e.preventDefault();
+
+        const templateParams = {
+            firstName,
+            lastName,
+            email,
+            emailMessage
+        }
+
+        const res = await fetch( "/api/form-submit", {
+            method : "POST",
+            headers: {
+                "Content-Type": "application/json", // Ensures JSON format
+            },
+            body : JSON.stringify( templateParams )
+        })
+
+        const data = await res.json();
+
+        ( data.invalidValues?.firstName ? setIsFirstNameInvalid( true ) : setIsFirstNameInvalid( false ) );
+
+        ( data.invalidValues?.emailAddress ? setIsEmailInvalid( true ) : setIsEmailInvalid( false ) );
+
+        ( data.invalidValues?.emailMessage  ? setIsMessageInvalid( true ) : setIsMessageInvalid( false ) );
+
+    }
     return (
 
         <section className='lg:flex-row lg:space-x-8 lg:space-y-0 space-y-8 wrapper-custom flex flex-col items-center section-pb'>
@@ -30,32 +72,41 @@ const Form = ({
                     </div>
                 }
             </div>
-            <form name="contact" method="POST" data-netlify="true" className='lg:w-1/2 w-full space-y-4'>
+            <form name="contact" method='POST' onSubmit={ ( e ) => { onFormSubmit ( e )} } className='lg:w-1/2 w-full space-y-4'>
                 <div className='md:flex-row flex-col flex gap-x-4'>
                     <div className='md:w-1/2 flex flex-col'>
+                        { isFirstNameInvalid &&
+                        <p>test</p>
+                        }
                         <label htmlFor='firstName' className='description-custom'>
                             First Name*
                         </label>
-                        <input type='text' name='firstName' id='firstName' required  className='standard-input'/>
+                        <input type='text' name='firstName' id='firstName' required  className='standard-input' onChange={ ( e ) => { setFirstName ( e.target.value )}}/>
                     </div>
                     <div className='md:w-1/2 flex flex-col'>
                         <label htmlFor='lastName' className='description-custom'>
                             Last Name
                         </label>
-                        <input type='text' name='lastName' id='lastName' className='standard-input'/>
+                        <input type='text' name='lastName' id='lastName' className='standard-input' onChange={ ( e ) => { setLastName ( e.target.value )}}/>
                     </div>
                 </div>
                 <div className='flex flex-col'>
+                    { isEmailInvalid &&
+                    <p>test</p>
+                    }
                     <label htmlFor='email' className='description-custom'>
                         Email*
                     </label>
-                    <input type='email' name='email' id='email' required className='standard-input' />
+                    <input type='email' name='email' id='email' required className='standard-input' onChange={ ( e ) => { setEmail ( e.target.value )}} />
                 </div>
                 <div className='flex flex-col'>
+                    { isMessageInvalid &&
+                    <p>test</p>
+                    }
                     <label htmlFor='message' className='description-custom'>
                         Message*
                     </label>
-                    <textarea name='message' id='message' required className='resize-none standard-input h-32'/>
+                    <textarea name='message' id='message' required className='resize-none standard-input h-32' onChange={ ( e ) => { setEmailMessage ( e.target.value )}}/>
                 </div>
                 <button type="submit" className='text-white standard-button scale-custom flex items-center text-lg'>
                     <span>
